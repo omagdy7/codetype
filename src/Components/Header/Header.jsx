8a79a92fe2d82a8e6f1c626a5fb4fc83e8c360e1
omagdy7 <omar.professional8777@gmail.com>
2022-12-07 15:51:42 +0200

    Added the main design components files and added daisyui

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-slate-600 bg-red-500 flex items-center justify-center">Hello there ya noooob!.</h1>
+      <button className="btn">I am button :)</button>
+    </div>
+  )
+}
+
+export default Header;
