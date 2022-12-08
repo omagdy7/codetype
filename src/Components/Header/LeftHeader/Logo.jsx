a6571486ca6ac0a19ceca31d09a712eefcc38cbc
omagdy7 <omar.professional8777@gmail.com>
2022-12-08 13:11:49 +0200

    Added the basic layout of the headerUI

---
+++ src/Components/Header/LeftHeader/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div className="text-2xl text-green-600">
+      CodeType
+    </div>
+  )
+}
+
+export default Logo;
---
+++ src/Components/Header/RightHeader/Avatar.jsx
@@ -0,0 +1,14 @@
+const Avatar = () => {
+  return (
+    <>
+    <div class="avatar placeholder ml-2">
+      <div class="bg-neutral-focus text-neutral-content rounded-full w-10">
+        <span class="text-xl">O</span>
+      </div>
+    </div> 
+    <span className="text-xl">omagdy7</span>
+    </>
+  )
+}
+
+export default Avatar;
