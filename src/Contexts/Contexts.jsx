e9f288fc218ecf018817041a884575faf903e16d
omagdy7 <omar.professional8777@gmail.com>
2022-12-13 21:00:12 +0200

    Deleted replicated files

--- src/Contexts/Contexts.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react'
-
-export const RaceContext = createContext({});
-export const IsTypingCanvasFocusedContext = createContext(true)
-export const CaretContext = createContext({});
-export const ColorContext = createContext("text-green-500");
-export const StatsContext = createContext({})
--- src/utils/getTotalChars.js
+++
@@ -1,8 +0,0 @@
-/* gets the total chars of the provided tets */
-export const getTotalChars = (test) => {
-  let totChars = 0;
-  test.forEach(element => {
-    totChars += element.line.length
-  });
-  return totChars
-}
